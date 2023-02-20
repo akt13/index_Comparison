21dab56a941ce6fc6c0be5b1e73b84ca4504f400
akt13 <abhijithkthoppil@gmail.com>
2023-02-21 00:15:21 +0530

    code cleaned

--- index_Comparison.py
+++ index_Comparison.py
@@ -3,21 +3,25 @@ import openpyxl
 import pandas as pd
 import yfinance as yf
 from glob import glob
-from datetime import timedelta, datetime,date
+from datetime import timedelta, datetime, date
+
 
 def find_indexPrice(row):
-    tradeDate=str(row['Trade Date'])
-    ticker=row['Symbol'] +'.NS'
+    tradeDate = str(row['Trade Date'])
+    ticker = row['Symbol'] + '.NS'
     print(ticker)
     format = '%Y-%m-%d'
     start = datetime.strptime(tradeDate, format)
     end = start + timedelta(days=1)
-    index_Price= yf.download('NIFTYBEES.NS',start,end)['Adj Close'].astype(int)
+    index_Price = yf.download('NIFTYBEES.NS', start, end)[
+        'Adj Close'].astype(int)
     print(index_Price[0])
     return index_Price[0]
 
+
 LTP_index = yf.Ticker('NIFTYBEES.NS').history(period='1d')
 index_LTP = LTP_index['Close'][0]
+
 
 def find_PnL(row):
     invest_Val = row['Price']*row['Quantity']
@@ -25,20 +29,22 @@ def find_PnL(row):
     index_PnL = indexCurrent_Val - invest_Val
     return invest_Val, index_PnL
 
-def pnl_Comparison(df,invested_val,present_val,unrealised_percentage):
+
+def pnl_Comparison(df, invested_val, present_val, unrealised_percentage):
     index_PnL = df['index_P&L'].sum()
     index_PnL_Percentage = (index_PnL/invested_val)*100
     print("Invest Value", invested_val)
-    print("Stock P&L",present_val)
-    print("Index P&L",index_PnL)
-    print("Stock P&L Percentage",unrealised_percentage)
-    print("Index P&L Percentage",index_PnL_Percentage)
+    print("Stock P&L", present_val)
+    print("Index P&L", index_PnL)
+    print("Stock P&L Percentage", unrealised_percentage)
+    print("Index P&L Percentage", index_PnL_Percentage)
+
 
 def portfolio_Change(row):
     # Get historical data for the past 135 days
     end_date = date.today()
     start_date = end_date - timedelta(days=200)
-    ticker=row['Symbol'] +'.NS'
+    ticker = row['Symbol'] + '.NS'
     data = yf.download(ticker, start=start_date, end=end_date)
 
     # Calculate the price change percentage for the last x days
@@ -47,23 +53,29 @@ def portfolio_Change(row):
     seven_days_data = data.tail(8)
     fourteen_days_data = data.tail(15)
     thirty_days_data = data.tail(31)
-    
+
     one_days_close = one_days_data['Close']
-    one_days_change = ((one_days_close.iloc[1] - row['Average Price'])/ row['Average Price']) * 100
-    
+    one_days_change = (
+        (one_days_close.iloc[1] - row['Average Price']) / row['Average Price']) * 100
+
     three_days_close = three_days_data['Close']
-    three_days_change = ((three_days_close.iloc[1] - row['Average Price'])/ row['Average Price']) * 100
-    
+    three_days_change = (
+        (three_days_close.iloc[1] - row['Average Price']) / row['Average Price']) * 100
+
     seven_days_close = seven_days_data['Close']
-    seven_days_change = ((seven_days_close.iloc[1] - row['Average Price'])/ row['Average Price']) * 100
-    
+    seven_days_change = (
+        (seven_days_close.iloc[1] - row['Average Price']) / row['Average Price']) * 100
+
     fourteen_days_close = fourteen_days_data['Close']
-    fourteen_days_change = ((fourteen_days_close.iloc[1] - row['Average Price'])/ row['Average Price']) * 100
-    
+    fourteen_days_change = (
+        (fourteen_days_close.iloc[1] - row['Average Price']) / row['Average Price']) * 100
+
     thirty_days_close = thirty_days_data['Close']
-    thirty_days_change = ((thirty_days_close.iloc[1] - row['Average Price'])/ row['Average Price']) * 100
-    
-    return one_days_change,three_days_change, seven_days_change, fourteen_days_change, thirty_days_change
+    thirty_days_change = (
+        (thirty_days_close.iloc[1] - row['Average Price']) / row['Average Price']) * 100
+
+    return one_days_change, three_days_change, seven_days_change, fourteen_days_change, thirty_days_change
+
 
 def print_sortedDF(df_holding):
     df_holding = df_holding.sort_values(by=['OneDay_Change'], ascending=False)
@@ -75,7 +87,8 @@ def print_sortedDF(df_holding):
     print('-------------------------------------------')
     print('------------------*******------------------')
 
-    df_holding = df_holding.sort_values(by=['ThreeDay_Change'], ascending=False)
+    df_holding = df_holding.sort_values(
+        by=['ThreeDay_Change'], ascending=False)
     print('Top 5 stocks in up-trend from last 3 day')
     print(df_holding[['Symbol', 'ThreeDay_Change']].head(5))
     print('-------------------------------------------')
@@ -84,7 +97,8 @@ def print_sortedDF(df_holding):
     print('-------------------------------------------')
     print('------------------*******------------------')
 
-    df_holding = df_holding.sort_values(by=['SevenDay_Change'], ascending=False)
+    df_holding = df_holding.sort_values(
+        by=['SevenDay_Change'], ascending=False)
     print('Top 5 stocks in up-trend from last 7 day')
     print(df_holding[['Symbol', 'SevenDay_Change']].head(5))
     print('-------------------------------------------')
@@ -93,7 +107,8 @@ def print_sortedDF(df_holding):
     print('-------------------------------------------')
     print('------------------*******------------------')
 
-    df_holding = df_holding.sort_values(by=['FourteenDay_Change'], ascending=False)
+    df_holding = df_holding.sort_values(
+        by=['FourteenDay_Change'], ascending=False)
     print('Top 5 stocks in up-trend from last 2 weeks')
     print(df_holding[['Symbol', 'FourteenDay_Change']].head(5))
     print('-------------------------------------------')
@@ -102,7 +117,8 @@ def print_sortedDF(df_holding):
     print('-------------------------------------------')
     print('------------------*******------------------')
 
-    df_holding = df_holding.sort_values(by=['ThirtyDay_Change'], ascending=False)
+    df_holding = df_holding.sort_values(
+        by=['ThirtyDay_Change'], ascending=False)
     print('Top 5 stocks in up-trend from last 1 month')
     print(df_holding[['Symbol', 'ThirtyDay_Change']].head(5))
     print('-------------------------------------------')
@@ -111,13 +127,15 @@ def print_sortedDF(df_holding):
     print('-------------------------------------------')
     print('------------------*******------------------')
 
+
 def main():
 
-    index_ClosePrices,stock_LTP_Lst,investVal_Lst,indexPnL_Lst = [],[],[],[]
-    oneDay_lst, threeDay_lst,sevenDay_lst,fourteenDay_lst,thirtyDay_lst = [],[],[],[],[]
-    
+    index_ClosePrices, stock_LTP_Lst, investVal_Lst, indexPnL_Lst = [], [], [], []
+    oneDay_lst, threeDay_lst, sevenDay_lst, fourteenDay_lst, thirtyDay_lst = [], [], [], [], []
+
     holdings_file = glob(("zerodha_exports/holdings-*.xlsx"))
-    df_holding = pd.read_excel(holdings_file[0], skiprows= 22 , usecols = ['Symbol','Average Price'])
+    df_holding = pd.read_excel(holdings_file[0], skiprows=22, usecols=[
+                               'Symbol', 'Average Price'])
     wb = openpyxl.load_workbook(holdings_file[0])
     sh = wb.active
     invested_val = sh['C15'].value
@@ -125,43 +143,46 @@ def main():
     unrealised_percentage = sh['C18'].value
 
     df = pd.DataFrame()
-    require_cols = ['Symbol', 'Trade Date','Trade Type','Quantity','Price','Order ID','Order Execution Time']
+    require_cols = ['Symbol', 'Trade Date', 'Trade Type',
+                    'Quantity', 'Price', 'Order ID', 'Order Execution Time']
     for f in glob("zerodha_exports/tradebook-*.xlsx"):
-        data = pd.read_excel(f, skiprows = 14, usecols = require_cols)
-        df = df.append(data,ignore_index=True)
+        data = pd.read_excel(f, skiprows=14, usecols=require_cols)
+        df = df.append(data, ignore_index=True)
     df.drop_duplicates('Order ID', inplace=True)
     df = df.drop(df[df['Trade Type'] == 'sell'].index)
     df.reset_index(inplace=True)
-    
-    for numb,row in df.iterrows():
+
+    for numb, row in df.iterrows():
         index_Price = find_indexPrice(row)
         index_ClosePrices.append(index_Price)
-    df["index_ClosePrices"] = index_ClosePrices 
+    df["index_ClosePrices"] = index_ClosePrices
 
-    for x,row in df.iterrows():
+    for x, row in df.iterrows():
         invest_Val, index_PnL = find_PnL(row)
         investVal_Lst.append(invest_Val)
         indexPnL_Lst.append(index_PnL)
-    df["invest_Value"] = investVal_Lst 
+    df["invest_Value"] = investVal_Lst
     df["index_P&L"] = indexPnL_Lst
 
-    pnl_Comparison(df,invested_val,present_val,unrealised_percentage)
+    pnl_Comparison(df, invested_val, present_val, unrealised_percentage)
 
-    for x,row in df_holding.iterrows():
-        one_days_change,three_days_change, seven_days_change, fourteen_days_change, thirty_days_change = portfolio_Change(row)
+    for x, row in df_holding.iterrows():
+        one_days_change, three_days_change, seven_days_change, fourteen_days_change, thirty_days_change = portfolio_Change(
+            row)
         oneDay_lst.append(one_days_change)
         threeDay_lst.append(three_days_change)
         sevenDay_lst.append(seven_days_change)
         fourteenDay_lst.append(fourteen_days_change)
         thirtyDay_lst.append(thirty_days_change)
-    df_holding["OneDay_Change"] = oneDay_lst 
-    df_holding["ThreeDay_Change"] = threeDay_lst 
-    df_holding["SevenDay_Change"] = sevenDay_lst 
-    df_holding["FourteenDay_Change"] = fourteenDay_lst 
-    df_holding["ThirtyDay_Change"] = thirtyDay_lst 
+    df_holding["OneDay_Change"] = oneDay_lst
+    df_holding["ThreeDay_Change"] = threeDay_lst
+    df_holding["SevenDay_Change"] = sevenDay_lst
+    df_holding["FourteenDay_Change"] = fourteenDay_lst
+    df_holding["ThirtyDay_Change"] = thirtyDay_lst
     df_holding.to_excel("output.xlsx")
 
     print_sortedDF(df_holding)
+
 
 if __name__ == '__main__':
     main()
